4d84bc72379396f3c5c18a7b9f60327267000083
Matt G <mattgauf@gmail.com>
2021-12-19 21:29:07 -0500

    Updated config according to breaking changes.

--- keyboards/gmmk/pro/ansi/keymaps/mattgauf/config.h
+++ keyboards/gmmk/pro/ansi/keymaps/mattgauf/config.h
@@ -34,34 +34,32 @@
 #define RGB_DISABLE_WHEN_USB_SUSPENDED true
 
 /* RGB Matrix Framebuffer Config */
-// #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-// #define RGB_MATRIX_TYPING_HEATMAP_DECREASE_DELAY_MS 25
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 
 /* RGB Matrix Disabled Effects */
-#define DISABLE_RGB_MATRIX_ALPHAS_MODS                      // Static dual hue, speed is hue for secondary hue
-#define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT                // Single hue 3 blade spinning pinwheel fades saturation
-#define DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL                // Single hue 3 blade spinning pinwheel fades brightness
-#define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL                // Full dual gradients scrolling out to in
-#define DISABLE_RGB_MATRIX_CYCLE_PINWHEEL                   // Full gradient spinning pinwheel around center of keyboard
-#define DISABLE_RGB_MATRIX_DUAL_BEACON                      // Full gradient spinning around center of keyboard
-#define DISABLE_RGB_MATRIX_RAINBOW_BEACON                   // Full tighter gradient spinning around center of keyboard
-#define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS                // Full dual gradients spinning two halfs of keyboard
-#define DISABLE_RGB_MATRIX_RAINDROPS                        // Randomly changes a single key's hue
-#define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS              // Randomly changes a single key's hue and saturation
-#define DISABLE_RGB_MATRIX_HUE_PENDULUM                     // Hue shifts up a slight ammount in a wave to the right, then back to the left
-#define DISABLE_RGB_MATRIX_HUE_WAVE                         // Hue shifts up a slight ammount and then back down in a wave to the right
+#undef ENABLE_RGB_MATRIX_ALPHAS_MODS                  // Static dual hue, speed is hue for secondary hue
+#undef ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT            // Single hue 3 blade spinning pinwheel fades saturation
+#undef ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL            // Single hue 3 blade spinning pinwheel fades brightness
+#undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL            // Full dual gradients scrolling out to in
+#undef ENABLE_RGB_MATRIX_CYCLE_PINWHEEL               // Full gradient spinning pinwheel around center of keyboard
+#undef ENABLE_RGB_MATRIX_DUAL_BEACON                  // Full gradient spinning around center of keyboard
+#undef ENABLE_RGB_MATRIX_RAINBOW_BEACON               // Full tighter gradient spinning around center of keyboard
+#undef ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS            // Full dual gradients spinning two halfs of keyboard
+#undef ENABLE_RGB_MATRIX_RAINDROPS                    // Randomly changes a single key's hue
+#undef ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS          // Randomly changes a single key's hue and saturation
+#undef ENABLE_RGB_MATRIX_HUE_PENDULUM                 // Hue shifts up a slight ammount in a wave to the right, then back to the left
+#undef ENABLE_RGB_MATRIX_HUE_WAVE                     // Hue shifts up a slight ammount and then back down in a wave to the right
+#undef ENABLE_RGB_MATRIX_PIXEL_RAIN                   // Randomly light keys with random hues
+#undef ENABLE_RGB_MATRIX_PIXEL_FRACTAL                // Single hue fractal filled keys pulsing horizontally out to edges
 
-#if defined(RGB_MATRIX_FRAMEBUFFER_EFFECTS)
-    #define DISABLE_RGB_MATRIX_DIGITAL_RAIN                 // That famous computer simulation
-#endif
+#undef ENABLE_RGB_MATRIX_TYPING_HEATMAP               // How hot is your WPM!
+#undef ENABLE_RGB_MATRIX_DIGITAL_RAIN                 // That famous computer simulation
 
-#if defined(RGB_MATRIX_KEYPRESSES) || defined(RGB_MATRIX_KEYRELEASES)
-    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE          // Hue & value pulse near a single key hit then fades value out
-    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS         // Hue & value pulse the same column and row of a single key hit then fades value out
-    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS         // Hue & value pulse away on the same column and row of a single key hit then fades value out
-    #define DISABLE_RGB_MATRIX_SPLASH                       // Full gradient & value pulse away from a single key hit then fades value out
-    #define DISABLE_RGB_MATRIX_SOLID_SPLASH                 // Hue & value pulse away from a single key hit then fades value out
-#endif
+#undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE          // Hue & value pulse near a single key hit then fades value out
+#undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS         // Hue & value pulse the same column and row of a single key hit then fades value out
+#undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS         // Hue & value pulse away on the same column and row of a single key hit then fades value out
+#undef ENABLE_RGB_MATRIX_SPLASH                       // Full gradient & value pulse away from a single key hit then fades value out
+#undef ENABLE_RGB_MATRIX_SOLID_SPLASH                 // Hue & value pulse away from a single key hit then fades value out
 
-#define DISABLE_RGB_MATRIX_EFFECT_MAX
+#undef ENABLE_RGB_MATRIX_EFFECT_MAX
 // clang-format on
